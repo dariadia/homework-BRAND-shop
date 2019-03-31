9ba0859fd1bd52e199d680f178a6af096a494a7f
Daria <daria.diachkova@mail.ru>
2019-03-31 23:49:55 +0300

    js page loading effect added

---
+++ zoomOnScroll.js
@@ -0,0 +1,41 @@
+"use strict";
+
+    function addOnWheel(elem, handler) {
+      if (elem.addEventListener) {
+        if ('onwheel' in document) {
+          // IE9+, FF17+
+          elem.addEventListener("wheel", handler);
+        } else if ('onmousewheel' in document) {
+          // устаревший вариант события
+          elem.addEventListener("mousewheel", handler);
+        } else {
+          // 3.5 <= Firefox < 17, более старое событие DOMMouseScroll пропустим
+          elem.addEventListener("MozMousePixelScroll", handler);
+        }
+      } else { // IE8-
+        text.attachEvent("onmousewheel", handler);
+      }
+    }
+
+    let scale = 1;
+    let ItemsZoomableArray = document.getElementsByClassName("mouseScrollZoom");
+    console.log(ItemsZoomableArray);
+
+    for (let i = 0; i < ItemsZoomableArray.length; i++) {
+let text = ItemsZoomableArray[i];
+
+    addOnWheel(text, function(e) {
+
+      let delta = e.deltaY || e.detail || e.wheelDelta;
+
+      // отмасштабируем при помощи CSS
+      if (delta > 0) scale += 0.05;
+      else scale -= 0.05;
+
+      text.style.transform = text.style.WebkitTransform = text.style.MsTransform = 'scale(' + scale + ')';
+
+      // отменим прокрутку
+      e.preventDefault();
+    });
+}
+
