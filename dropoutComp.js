45691058be53bcaed467f89a6c763d10de872b68
Daria <daria.diachkova@mail.ru>
2019-04-16 19:11:52 +0300

    updating vue js

--- dropoutComp.js
+++ dropoutComp.js
@@ -21,32 +21,9 @@ Vue.component('dropout', {
    // methods: {},
     template: `<li class="menu-list"><a href="product.html" class="menu-link">{{title}}</a>
     <div class="drop drop-second">
-        <div class="drop-flex">
-            <h3 class="drop-h3">WOMEN</h3>
-            <ul class="drop-ul">
-                <li><a href="#" class="drop-link">Dresses</a></li>
-                <li><a href="#" class="drop-link">Sweaters/Knits</a></li>
-                <li><a href="#" class="drop-link">Jackets/Coats</a></li>
-                <li><a href="#" class="drop-link">Blazers</a></li>
-                <li><a href="#" class="drop-link">Denim</a></li>
-                <li><a href="#" class="drop-link">Leggings/Pants</a></li>
-                <li><a href="#" class="drop-link">Skirts/Shorts</a></li>
-                <li><a href="#" class="drop-link">Accessories</a></li>
-                <li><a href="#" class="drop-link">Bags/Purses</a></li>
-                <li><a href="#" class="drop-link">Swimwear/Underwear</a></li>
-                <li><a href="#" class="drop-link">Nightwear</a></li>
-            </ul>
-        </div>
+        <dropout-lg-ul></dropout-lg-ul>
         <div>
-            <div class="drop-flex">
-                <h3 class="drop-h3">WOMEN</h3>
-                <ul class="drop-ul">
-                    <li><a href="#" class="drop-link">Dresses</a></li>
-                    <li><a href="#" class="drop-link">Tops</a></li>
-                    <li><a href="#" class="drop-link">Sweaters/Knits</a></li>
-                    <li><a href="#" class="drop-link">Jackets/Coats</a></li>
-                </ul>
-            </div>
+        <dropout-ul></dropout-ul>
             <div class="drop-flex second-element">
                 <h3 class="drop-h3">WOMEN</h3>
                 <ul class="drop-ul">
@@ -57,19 +34,41 @@ Vue.component('dropout', {
             </div>
         </div>
         <div>
-            <div class="drop-flex">
-                <h3 class="drop-h3">WOMEN</h3>
-                <ul class="drop-ul">
-                    <li><a href="#" class="drop-link">Dresses</a></li>
-                    <li><a href="#" class="drop-link">Tops</a></li>
-                    <li><a href="#" class="drop-link">Sweaters/Knits</a></li>
-                    <li><a href="#" class="drop-link">Jackets/Coats</a></li>
-                </ul>
-            </div>
+                <dropout-ul></dropout-ul>
             <div class="second-element jello-horizontal"><a class="super-sale" href="#"><img src="img/super-sale.png" alt="super sale banner">
                     <h3 class="super-sale-text">SUPER SALE</h3>
                 </a></div>
         </div>
     </div>
 </li>`
+});
+
+
+Vue.component('dropout-ul', {
+    data(){
+        return {
+            list: ['Dresses', 'Tops', 'Sweaters/Knits', 'Jackets/Coats'],
+        }
+    },
+    template: `<div class="drop-flex">
+    <h3 class="drop-h3">WOMEN</h3>
+    <ul class="drop-ul">
+                <li v-for="item in list"><a href="#" class="drop-link">{{item}}</a></li>
+             </ul>
+             </div>`
+});
+
+
+Vue.component('dropout-lg-ul', {
+    data(){
+        return {
+            list: ['Dresses', 'Tops', 'Sweaters/Knits', 'Jackets/Coats', 'Blazers', 'Denim', 'Leggings/Pants', 'Skirts/Shorts', 'Accessories', 'Bags/Purses', 'Swimwear/Underwear', 'Nightwear'],
+        }
+    },
+    template: `<div class="drop-flex">
+    <h3 class="drop-h3">WOMEN</h3>
+    <ul class="drop-ul">
+                <li v-for="item in list"><a href="#" class="drop-link">{{item}}</a></li>
+             </ul>
+             </div>`
 });
